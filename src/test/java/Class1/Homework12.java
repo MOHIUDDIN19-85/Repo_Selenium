4c55eba833e12ed4f1357b8a096b19e845d86f5d
Technosoft academy 008 <technosoftacademy008@Technosofts-MacBook-Pro-2.local>
2020-12-02 16:38:33 -0500

    H12

--- src/test/java/Class1/Homework12.java
+++ src/test/java/Class1/Homework12.java
@@ -8,7 +8,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Homework12 {
-    @Test
+   /** @Test
     public void verifySummaruyTemp(){
         /**
          * Launch darksky.net
@@ -16,16 +16,42 @@ public class Homework12 {
          * get the text from the element
          * verify temp value is more than 10
          */
-        System.setProperty("webdriver.chrome.driver","./DriverExe/chromedriver");
+        /**System.setProperty("webdriver.chrome.driver","./DriverExe/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.darksky.net");
-        String currentTemp = "//span[@class='summary swap']";
-        By currentTempLocator = By.xpath(currentTemp);
+        String feelsLike = "//span[@class='feels-like-text']";
+        By currentTempLocator = By.xpath(feelsLike);
         WebElement currentTempWE = driver.findElement(currentTempLocator);
         String currentTempText =currentTempWE.getText();
         String[] spTemp =currentTempText.split("˚");
-        int currTempInt = Integer.valueOf(spTemp[0]);
-        Assert.assertTrue(currTempInt>10,"current temp is less then 10");
+        int feelTempInt = Integer.valueOf(spTemp[0]);
+        System.out.println(feelTempInt);
+        WebElement lowTemp = driver.findElement(By.xpath("//span[@class='low-temp-text']"));
+        String lowTempText = lowTemp.getText();
+        String[] lwTemp = lowTempText.split("˚");
+        int lowTempInt =Integer.valueOf(lwTemp[0]);
+        System.out.println(lowTempInt);
+        WebElement highTemp = driver.findElement(By.xpath("//span[@class='high-temp-text']"));
+        String highTempText = highTemp.getText();
+        String[] hgTemp = highTempText.split("˚");
+        int highTempInt = Integer.valueOf(hgTemp[0]);
+        System.out.println(highTempInt);
+        Assert.assertTrue(feelTempInt>lowTempInt,"Feel temp is not greater than low temp");
+        Assert.assertTrue(feelTempInt<highTempInt,"Feel temp is not less than high temp");
+
+    }*/
+    //testcase-3
+    @Test
+    public void verifyDates(){
+        System.setProperty("webdriver.chrome.driver","./DriverExe/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.darksky.net");
+        WebElement tempInFarenheight = driver.findElement(By.xpath("//span[@class='summary swap']"));
+        String tempFText =tempInFarenheight.getText();
+        String[] tempFText1 = tempFText.split("˚");
+        int tempFInt = Integer.valueOf(tempFText1[0]);
+        WebElement tempSelect = driver.findElement(By.xpath("//b[@class='button']/ancestor::div[@class='options']/preceding::form[@id='searchForm']"));
+        tempSelect.click();
     }
 
 }
